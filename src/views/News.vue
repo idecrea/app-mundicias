9f13745651c33bb8de7ff8157a8e5a9db5984d69
Josep Chanza <josepchanza@gmail.com>
2019-03-11 12:49:45 +0100

    Rellenar News

--- src/views/News.vue
+++ src/views/News.vue
@@ -2,41 +2,96 @@
   <div class="news">
     <div class="header">
       <router-link class="header__regresar" :to="{ name:'list' }"></router-link>
-      <div class="header__data">
+      <div v-if="baseDatos.length > 0" class="header__data">
         <div class="header__fecha">
           <p>
-            12/11/2018
+            {{noticia.pubDate | tiempoTranscurrido}}
           </p>
         </div>
         <div class="header__categoria">
           <p>
-            Categoría
+             {{ noticia.categories[0] }}
           </p>
         </div>
         <div class="header__duracion">
-          <p>
-            Duración de lectura: 2 min
+          <p v-if="noticia.content !=''">
+           Tiempo lectura : {{noticia.content | quitarHTML | tiempoLectura}} minutos
           </p>
         </div>
       </div>
     </div>
     <div class="news">
-      <div class="news__main">
+      <div class="news__main" :style="`background-image:url(${noticia.thumbnail})`">
         <div class="news__main-title">
           <h1 class="news__main-title-text">
-            Titulo noticia
+            {{noticia.title}}
           </h1>
           <p class="news__main-title-author">
-            Autor de la noticia
+             {{noticia.author}}
           </p>
         </div>
       </div>
       <div class="news__noticia">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus suscipit esse eaque, quis neque laborum eius, nesciunt dolorum similique temporibus ducimus dolor cum soluta, et maxime rerum molestiae reiciendis voluptate?</p>
-        <p>Magni excepturi repellat corrupti est quo nemo asperiores quis iste mollitia! Ut, molestias! Officia, neque mollitia? Unde doloremque eaque nihil soluta dignissimos! Quod mollitia provident nemo iste minima dolorem officiis!</p>
-        <p>Nobis accusantium modi, temporibus facere minima soluta consequuntur sunt obcaecati quisquam consectetur molestiae praesentium et. Molestias maiores ea laboriosam explicabo cupiditate deserunt sed doloribus animi aut pariatur, officia, amet facere.</p>
+        <p>{{noticia.content | quitarHTML}}</p>
       </div>
-      <a href="#" class="news__enlace-fuente">Enlace a la noticia original</a>
+      <a :href="noticia.link" class="news__enlace-fuente" target="_blank">Enlace a la noticia original</a>
     </div>
   </div>
 </template>
+
+<script>
+
+import moment from 'moment';
+
+moment.locale('es');
+
+export default {
+  name : 'news',
+  components : {
+
+  },
+  data : function (){
+    return {
+       posicionPasada : 0,
+       baseDatos : [],
+       noticia : ''
+    }
+  },
+  mounted : function(){
+
+    let datos = JSON.parse(localStorage.getItem("baseDatos"));
+
+    if(datos != null){
+      this.baseDatos = datos.bd;
+    }
+    this.posicionPasada = this.$route.params.pos;
+    
+    this.noticia = this.baseDatos[this.posicionPasada];
+
+    console.log(this.noticia);
+
+  },
+  filters :{
+      tiempoTranscurrido : function(fecha){  
+
+        let tiempo = moment(`${fecha}`).fromNow();
+
+        return tiempo;
+      },
+      quitarHTML : function(texto){
+
+          let final = texto != undefined ? texto.replace(/<[^>]+>/g, '') : '';
+          return final;
+     },
+      tiempoLectura : function(texto){
+
+       let palabras_minuto = 150;
+       texto = texto.split(' ');
+
+       return Math.ceil(texto.length / palabras_minuto);
+
+     }
+    }
+  }
+</script>
+
