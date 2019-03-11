2409ddf948a9bd1de812d28e77948a074199ede3
root <root@DESKTOP-I1RLS17.localdomain>
2019-03-11 13:21:09 +0100

    add loading

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,155 +1,174 @@
 <template>
   <div class="listContainer">
-    <div class="cabecera-inicio" :style="`background-image : url(${imagenPrimera});background-position : center center;background-size : cover;background-repeat : no-repeat`">
-      <img src="/img/mundicio.svg" alt="" class="logo-mundicio">
+    <div
+      class="cabecera-inicio"
+      :style="`background-image : url(${imagenPrimera});background-position : center center;background-size : cover;background-repeat : no-repeat`"
+    >
+      <img v-if="!loading" src="/img/mundicio.svg" alt class="logo-mundicio">
+      <img v-if="!loading" src="/img/icono-menu.svg" alt class="icono-menu">
+      <div v-if="loading" class="cargando">
+        <Loading></Loading>
+        <p class="parrafo">Cargando noticias ...</p>
+      </div>
 
-      <img src="/img/icono-menu.svg" alt="" class="icono-menu">
-      <div>
-          <transition name="fade">
-            <input v-if="mostrar" type="text" v-model="busqueda" placeholder="Titulo de la noticia" class="input">
-          </transition>
-        <img @click="mostrar = !mostrar" src="/img/search.svg" alt="" class="icono-menu2">
+      <div v-if="!loading">
+        <transition name="fade">
+          <input
+            v-if="mostrar"
+            type="text"
+            v-model="busqueda"
+            placeholder="Titulo de la noticia"
+            class="input"
+          >
+        </transition>
+        <img @click="mostrar = !mostrar" src="/img/search.svg" alt class="icono-menu2">
       </div>
     </div>
     <div v-for="(noticia,pos) in arrayNoticias" :key="noticia.id" class="noticia">
-      <router-link :to="{ name : 'news',params : {pos : pos} }" v-if="cleanHTML(noticia.content) != ''">
-          <h1 class="noticia__titulo">{{ noticia.title }}</h1>
-          <!--<p>{{ noticia.content | quitarHTML}}</p>-->
-          <ol class="noticia__categoria">
-              <li v-for="categoria in noticia.categories" :key="categoria.id">{{ categoria }}</li>
-          </ol>
-          <!--<img :src="noticia.thumbnail">-->
-          <div class="noticia__tiempo">
-            <p>{{ noticia.pubDate | tiempoTranscurrido}}</p>
-            <p> Tiempo lectura : {{noticia.content | quitarHTML | tiempoLectura}} minutos</p>
-          </div>
+      <router-link
+        :to="{ name : 'news',params : {pos : pos} }"
+        v-if="cleanHTML(noticia.content) != ''"
+      >
+        <h1 class="noticia__titulo">{{ noticia.title }}</h1>
+        <!--<p>{{ noticia.content | quitarHTML}}</p>-->
+        <ol class="noticia__categoria">
+          <li v-for="categoria in noticia.categories" :key="categoria.id">{{ categoria }}</li>
+        </ol>
+        <!--<img :src="noticia.thumbnail">-->
+        <div class="noticia__tiempo">
+          <p>{{ noticia.pubDate | tiempoTranscurrido}}</p>
+          <p>Tiempo lectura : {{noticia.content | quitarHTML | tiempoLectura}} minutos</p>
+        </div>
       </router-link>
     </div>
   </div>
 </template>
 
 <script>
+import Loading from "@/components/Loading.vue";
+import moment from "moment";
+import { constants } from "fs";
 
-import moment from 'moment';
-import { constants } from 'fs';
-
-moment.locale('es');
+moment.locale("es");
 
 export default {
-
-  name: 'list',
+  name: "list",
   components: {
-    moment
+    moment,
+    Loading
   },
-  data : function(){
+  data: function() {
     return {
-        arrayNoticias : [],
-        totalResultados : 0,
-        mostrar : false,
-        imagenPrimera : ''
-    }
+      arrayNoticias: [],
+      totalResultados: 0,
+      mostrar: false,
+      imagenPrimera: "",
+      loading: true
+    };
   },
-  mounted : function(){
+  mounted: function() {
+    this.getNews();
 
-      this.getNews();
+    let datos = JSON.parse(localStorage.getItem("baseDatos"));
+  },
+  methods: {
+    getNews: function() {
+      let that = this;
+      let url =
+        "https://cors-anywhere.herokuapp.com/https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.abc.es%2Frss%2Ffeeds%2Fabc_ultima.xml";
 
-      let datos = JSON.parse(localStorage.getItem("baseDatos"));
+      axios
+        .get(url)
+        .then(function(response) {
+          // handle success
 
+          that.cleanArray(response.data.items);
 
-
-  },methods:{
-    getNews : function(){
-     
-     let that = this;
-    let url = 'https://cors-anywhere.herokuapp.com/https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.abc.es%2Frss%2Ffeeds%2Fabc_ultima.xml';
-    
-    axios.get(url)
-        .then(function (response) {
-        // handle success
-
-        that.cleanArray(response.data.items);
-
-        let bd = {
-            "bd" : that.arrayNoticias
-        };
-        localStorage.setItem("baseDatos", JSON.stringify(bd));
-        
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      });
+          let bd = {
+            bd: that.arrayNoticias
+          };
+          localStorage.setItem("baseDatos", JSON.stringify(bd));
+          that.loading = false;
+        })
+        .catch(function(error) {
+          // handle error
+          console.log(error);
+        })
+        .then(function() {
+          // always executed
+        });
     },
-    cleanHTML : function(texto){
-      
+    cleanHTML: function(texto) {
       return this.$options.filters.quitarHTML(texto);
-
     },
-    cleanArray (noticias){
-      
-      for(let item of noticias){
-        if(this.cleanHTML(item.content) != ''){
-            this.arrayNoticias.push(item);
-            this.imagenPrimera = this.arrayNoticias[0].thumbnail;
-        }else{
-
+    cleanArray(noticias) {
+      for (let item of noticias) {
+        if (this.cleanHTML(item.content) != "") {
+          this.arrayNoticias.push(item);
+          this.imagenPrimera = this.arrayNoticias[0].thumbnail;
+        } else {
         }
       }
       console.log(this.arrayNoticias);
     }
-    ,
-  },filters :{
-     tiempoTranscurrido : function(fecha){
-           
-        let tiempo = moment(`${fecha}`).fromNow();
+  },
+  filters: {
+    tiempoTranscurrido: function(fecha) {
+      let tiempo = moment(`${fecha}`).fromNow();
 
-        return tiempo;
-     },
-     quitarHTML : function(texto){
+      return tiempo;
+    },
+    quitarHTML: function(texto) {
+      return texto.replace(/<[^>]+>/g, "");
+    },
+    tiempoLectura: function(texto) {
+      let palabras_minuto = 150;
+      texto = texto.split(" ");
 
-       return texto.replace(/<[^>]+>/g, '');
-     },
-     tiempoLectura : function(texto){
-
-       let palabras_minuto = 150;
-       texto = texto.split(' ');
-
-        
-       return Math.ceil(texto.length / palabras_minuto);
-
-
-     }
+      return Math.ceil(texto.length / palabras_minuto);
+    }
   }
-}
+};
 </script>
 
 
 <style scoped>
-.listContainer{
+.listContainer {
   width: 100vw;
   height: 100vh;
 }
 
-.icono-menu2{
-    position: absolute;
-    width: 30px;
-    padding: 1rem;
-    right: 100px;
-    border: 1px solid black;
-    cursor: pointer;
+.icono-menu2 {
+  position: absolute;
+  width: 30px;
+  padding: 1rem;
+  right: 100px;
+  border: 1px solid black;
+  cursor: pointer;
 }
-.input{
+.input {
   border: 1px solid black;
 }
 
-.fade-enter-active, .fade-leave-active {
+.fade-enter-active,
+.fade-leave-active {
   transition: opacity 1s;
 }
-.fade-enter, .fade-leave-to{
+.fade-enter,
+.fade-leave-to {
   opacity: 0;
+}
+
+.cargando {
+  position: absolute;
+  left: 59%;
+  top: 110%;
+  transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+}
+
+.parrafo {
+  color: white;
 }
 </style>
 
