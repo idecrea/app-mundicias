6c9512ebd996b159690035f4b6150e01807d460f
Valentina <v.rubane@gmail.com>
2019-03-11 15:07:53 +0100

    anyadiendo animacion

--- src/views/News.vue
+++ src/views/News.vue
@@ -2,7 +2,7 @@
   <div class="news">
     <div class="header">
       <router-link class="header__regresar" :to="{ name:'list' }">
-        <object id="my-svg" type="image/svg+xml" data="/img/icono-atras.svg"></object>
+        <div class="header__regresar-flecha anime-inicio" id="flecha" :class="{'anime-inicio'}"></div>
       </router-link>
       <div v-if="baseDatos.length > 0" class="header__data">
         <div class="header__fecha">
@@ -15,9 +15,9 @@
              {{ noticia.categories[0] }}
           </p>
         </div>
-        <div class="header__duracion">
+        <div class="header__duracion" >
           <p v-if="noticia.content !=''">
-           Tiempo lectura : {{noticia.content | quitarHTML | tiempoLectura}} minutos
+           Tiempo lectura : {{noticia.content | quitarHTML | tiempoLectura}} min
           </p>
         </div>
       </div>
@@ -44,14 +44,12 @@
 <script>
 
 import moment from 'moment';
-import vueSvgDraw from 'vue-svg-draw'
 
 moment.locale('es');
 
 export default {
   name : 'news',
   components : {
-
   },
   data : function (){
     return {
@@ -72,6 +70,16 @@ export default {
     this.noticia = this.baseDatos[this.posicionPasada];
 
     console.log(this.noticia);
+
+    let vivus = new Vivus('my-svg', {duration: 2000});
+
+    setTimeout(function () {
+          let flecha = document.querySelector('#flecha')
+
+      flecha.classList.remove('anime-inicio')
+      flecha.classList.add('anime')
+    }, 1250)
+
 
   },
   filters :{
